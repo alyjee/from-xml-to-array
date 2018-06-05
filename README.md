50ea0ef24cd330fff5cd33c59dff2b4d8b6a088e
Tahir <tahir.raza@ilsainteractive.com>
2018-06-05 12:56:16 +0500

    package published

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# from-xml-to-array
+# Conversion from xml to an array
 A package inspired by vyuldashev/xml-to-array to convert an xml to array
+
+Inspired by Vyuldashev's [xml-to-array](https://github.com/vyuldashev/xml-to-array) ❤️ 
+
+## Install
+
+You can install this package via composer.
+
+``` bash
+composer require alyjee/from-xml-to-array
+```
+
+## Usage
+
+```php
+use Alyjee\XmlToArray\XmlToArray;
+
+$xml = '<items>
+	<Facilities>
+		<Facility Code="*EC"><![CDATA[Earliest check-in at 14:00]]></Facility>
+		<Facility Code="*LF"><![CDATA[1 lift]]></Facility>
+		<Facility Code="*RS"><![CDATA[Room Service from 18:00 to 21:00]]></Facility>
+	</Facilities>
+	</items>';
+
+$result = XmlToArray::convert($xml);
+```
+After running this piece of code `$result` will contain:
+
+```php
+Array
+(
+    [items] => Array
+        (
+            [Facilities] => Array
+                (
+                    [Facility] => Array
+                        (
+                            [0] => Array
+                                (
+                                    [_attributes] => Array
+                                        (
+                                            [Code] => *EC
+                                        )
+
+                                    [_cdata] => Earliest check-in at 14:00
+                                )
+
+                            [1] => Array
+                                (
+                                    [_attributes] => Array
+                                        (
+                                            [Code] => *LF
+                                        )
+
+                                    [_cdata] => 1 lift
+                                )
+
+                            [2] => Array
+                                (
+                                    [_attributes] => Array
+                                        (
+                                            [Code] => *RS
+                                        )
+
+                                    [_cdata] => Room Service from 18:00 to 21:00
+                                )
+
+                        )
+
+                )
+
+        )
+
+)
+```
---
+++ tests/XmlToArrayTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alyjee\XmlToArray\Test;
+
+use PHPUnit\Framework\TestCase;
+use Spatie\ArrayToXml\ArrayToXml;
+use Alyjee\XmlToArray\XmlToArray;
+
+class XmlToArrayTest extends TestCase
+{
+    /** @dataProvider data
+     * @param array $array
+     */
+    public function test(array $array)
+    {
+        $xml = ArrayToXml::convert($array, 'items');
+        $this->assertSame(['items' => $array], XmlToArray::convert($xml));
+    }
+
+    public function data()
+    {
+        return [
+            [
+                [
+                    'good_guy' => [
+                        'name' => 'Luke Skywalker',
+                        'weapon' => 'Lightsaber',
+                    ],
+                    'bad_guy' => [
+                        'name' => 'Sauron',
+                        'weapon' => 'Evil Eye',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'good_guy' => [
+                        '_attributes' => ['attr1' => 'value'],
+                        'name' => 'Luke Skywalker',
+                        'weapon' => 'Lightsaber',
+                    ],
+                    'bad_guy' => [
+                        'name' => 'Sauron',
+                        'weapon' => 'Evil Eye',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'good_guy' => [
+                        'name' => [
+                            '_cdata' => '<h1>Luke Skywalker</h1>',
+                        ],
+                        'weapon' => 'Lightsaber',
+                    ],
+                    'bad_guy' => [
+                        'name' => '<h1>Sauron</h1>',
+                        'weapon' => 'Evil Eye',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /** @dataProvider sameNameData
+     * @param array $array
+     * @test
+     */
+    public function sameNameTest(array $array)
+    {
+        $xml = ArrayToXml::convert($array, 'items');
+        $convertedArr = XmlToArray::convert($xml);
+        $this->assertSame(['items' => $array], XmlToArray::convert($xml));
+    }
+
+    public function sameNameData()
+    {
+        return [
+            [
+                [
+                    'Facilities' => [
+                        'Facility' => [
+                            [
+                                '_attributes' => ['Code'=>'*EC'],
+                                '_cdata' =>  'Earliest check-in at 14:00',
+                            ],
+                            [
+                                '_attributes' => ['Code'=>'*LF'],
+                                '_cdata' =>  '1 lift',
+                            ],
+                            [
+                                '_attributes' => ['Code'=>'*RS'],
+                                '_cdata' =>  'Room Service from 18:00 to 21:00',
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ];
+    }
+}
